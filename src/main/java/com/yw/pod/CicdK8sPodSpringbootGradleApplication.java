75ff5f5c5b4e8ad97ab3b06e3368d90309f649b9
dsudnr96 <dsudnr96@gmail.com>
2025-05-24 22:03:36 +0900

    init

---
+++ src/main/java/com/yw/pod/CicdK8sPodSpringbootGradleApplication.java
@@ -0,0 +1,13 @@
+package com.yw.pod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdK8sPodSpringbootGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdK8sPodSpringbootGradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yw/pod/CicdK8sPodSpringbootGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yw.pod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdK8sPodSpringbootGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
